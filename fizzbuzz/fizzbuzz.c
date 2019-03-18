400b6bec786e1a3a2262517244ff595e516d91ca
theryanclausen <theryanclausen@gmail.com>
2019-03-18 15:10:00 -0700

    fizzbuzz remove duplicate

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -8,52 +8,33 @@
     every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
-// int fizzbuzz(int n)
-// {
-//  int i;
-//  int counter = 0;
-//  for(i=1; i <= n; i++)
-//  {
-//  if(!(i%3) && !(i%5))
-//  {
-//      printf("FizzBuzz\n");
-//  }
-// if(!(i%3))
-//  {
-//      printf("Fizz\n");
-//  }
-// if(!(i%5))
-//  {
-//      printf("Buzz\n");
-//  }
-//  else
-//  {
-//      counter = counter + 1;
-//  }
-//  }
-//  return counter;
-// }
-
 int fizzbuzz(int n)
 {
-  int i;
-  int counter = 0;
-
-  for( i = 1; i <= n; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      if (i % 3 == 0) {
-        printf("Fizz");
-      }
-      if (i % 5 == 0) {
-        printf("Buzz");
-      }
-      printf("\n");
-      } else {
-        counter++;
-      }
-  }
-  return counter;
+ int i;
+ int counter = 0;
+ for(i=1; i <= n; i++)
+ {
+ if(!(i%3) && !(i%5))
+ {
+     printf("FizzBuzz\n");
+ }
+if(!(i%3))
+ {
+     printf("Fizz\n");
+ }
+if(!(i%5))
+ {
+     printf("Buzz\n");
+ }
+ else
+ {
+     counter = counter + 1;
+ }
+ }
+ return counter;
 }
+
+
 
 #ifndef TESTING
 int main(void)
